7d17af02df6af97d0590442b54afc9ff9ce0ff86
Faith Odonghanro <toriboi.fo@gmail.com>
2024-05-06 16:02:20 +0100

    Add more basic tests

--- src/test/java/com/dronetech/app/DroneTechAppApplicationTests.java
+++ src/test/java/com/dronetech/app/DroneTechAppApplicationTests.java
@@ -1,9 +1,10 @@
 package com.dronetech.app;
 
 import com.dronetech.app.dtos.DroneDto;
-import com.dronetech.app.entities.mappers.DroneEntityMapper;
+import com.dronetech.app.dtos.MedicationDto;
 import com.dronetech.app.respositories.DroneRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,7 +18,6 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import uk.co.jemos.podam.api.PodamFactory;
 import uk.co.jemos.podam.api.PodamFactoryImpl;
 
-import static org.hamcrest.Matchers.contains;
 import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -26,11 +26,12 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = DroneTechAppApplication.class)
 @AutoConfigureMockMvc
+@Slf4j
 class DroneTechAppApplicationTests {
 
     PodamFactory podam = new PodamFactoryImpl();
     ObjectMapper mapper = new ObjectMapper();
-    DroneDto droneDto = podam.manufacturePojo(DroneDto.class);
+    DroneDto globalDroneDto = podam.manufacturePojo(DroneDto.class);
 
     @Autowired
     MockMvc mockMvc;
@@ -77,7 +78,7 @@ class DroneTechAppApplicationTests {
                 .content(mapper.writeValueAsString(droneDto)))
             .andExpect(status().isBadRequest())
             .andExpect(jsonPath("success", is(false)))
-            .andExpect(jsonPath("message", containsString("is already registered")));
+            .andExpect(jsonPath("errorCode", containsString("DRONE_ALREADY_REGISTERED")));
     }
 
     @WithMockUser(value = "spring")
@@ -92,19 +93,82 @@ class DroneTechAppApplicationTests {
                 .content(mapper.writeValueAsString(droneDto)))
             .andExpect(status().isBadRequest())
             .andExpect(jsonPath("success", is(false)))
-            .andExpect(jsonPath("message", is("Battery Capacity cannot be below 25 when Drone State is LOADING")));
+            .andExpect(jsonPath("errorCode", is("DRONE_STATE_BATTERY_MISMATCH")));
     }
 
     @WithMockUser(value = "spring")
     @Test
-    void retrieveDrone() throws Exception {
-        DroneDto droneDto = podam.manufacturePojo(DroneDto.class);
-        droneRepository.save(DroneEntityMapper.dtoToEntity(droneDto));
+    void test_retrieveDrone_successfully() throws Exception {
+        String sn001 = "SN001";
 
-        mockMvc.perform(MockMvcRequestBuilders.get("/v1/drone/" + droneDto.getSerialNo()))
+        mockMvc.perform(MockMvcRequestBuilders.get("/v1/drone/" + sn001))
             .andExpect(status().isOk())
             .andExpect(jsonPath("success", is(true)));
     }
 
+    @WithMockUser(value = "spring")
+    @Test
+    void test_retrieveDrone_nonExistingDrone() throws Exception {
+        String nonExistingSn = "nonExistingSn";
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/v1/drone/" + nonExistingSn))
+            .andExpect(status().isBadRequest())
+            .andExpect(jsonPath("success", is(false)))
+            .andExpect(jsonPath("errorCode", is("DRONE_NOT_FOUND")));
+    }
+
+    @WithMockUser(value = "spring")
+    @Test
+    void test_retrieveAllDrones_successfully() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/v1/drone/all"))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("success", is(true)))
+            .andExpect(jsonPath("$.data.length()", is(12)));
+    }
+
+    @WithMockUser(value = "spring")
+    @Test
+    void test_loadMedications_dtoValidation() throws Exception {
+        MedicationDto medicationDto = podam.manufacturePojo(MedicationDto.class);
+        String sn002 = "SN002";
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/v1/drone/" + sn002 + "/medications")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(mapper.writeValueAsString(medicationDto)))
+            .andExpect(status().isBadRequest())
+            .andExpect(jsonPath("success", is(false)))
+            .andExpect(jsonPath("message", is("Validation error")));
+    }
+
+    @WithMockUser(value = "spring")
+    @Test
+    void test_retrieveLoadedMedications() throws Exception {
+        String sn002 = "SN002";
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/v1/drone/" + sn002 + "/medications"))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("success", is(true)))
+            .andExpect(jsonPath("$.data.length()", is(2)));
+    }
+
+    @WithMockUser(value = "spring")
+    @Test
+    void test_retrieveAvailableDrones() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/v1/drone/all"))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("success", is(true)))
+            .andExpect(jsonPath("$.data.length()", is(12)));
+    }
+
+    @WithMockUser(value = "spring")
+    @Test
+    void test_retrieveDroneBatteryLevel() throws Exception {
+        String sn002 = "SN002";
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/v1/drone/" + sn002 + "/battery"))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("success", is(true)))
+            .andExpect(jsonPath("data", is(25)));
+    }
 
 }
